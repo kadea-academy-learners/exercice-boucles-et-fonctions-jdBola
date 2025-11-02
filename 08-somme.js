44b13008ba28d2b4532b6687dbccd27eea7fb854
Jean Dino <boladino56@gmail.com>
2025-11-02 18:23:15 +0100

    08-somme ok

--- 08-somme.js
+++ 08-somme.js
@@ -7,13 +7,25 @@
   Signature attendue :
     function somme(n) -> number
 
-  Placeholder : levée d'erreur pour indiquer que l'exercice doit être implémenté.
-*/
+  Placeholder : levée d'erreur pour indiquer que l'exercice doit être implémenté.*/
 function somme(n) {
   // Exercice non implémenté : retourner la somme de 1 à n
   // Placeholder neutre : retourne 0 pour indiquer non-implémentation
-  return 0;
+  if(isNaN(n) || typeof n !=='number'){
+    return null
+  }
+  let total = 0;
+  for(let i = 1; i<= n ; i++){
+    total = total + i;
+  }
+  return total;
 }
+
+console.log(somme(5))
+console.log(somme(10))
+console.log(somme('papa'))
+console.log(somme())
+
 
 // Ne pas modifier la ligne ci-dessous
 module.exports = { somme }
